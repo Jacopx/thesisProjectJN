4643be308550fd3a8ec52492fa21924b45f6b044
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-09-25 14:56:04 +0200

    Adding heatpmap visualization and cleaning dataset

--- DataAnalysis_VVF/main.py
+++ DataAnalysis_VVF/main.py
@@ -1,8 +1,10 @@
 import time
-import numpy
+import numpy as np
 import pandas as pd
 import math
 import scipy
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 loc = {}
 typo = {}
@@ -43,18 +45,41 @@ def extract_feature(df):
     return df
 
 
+def convert_time(df):
+    v = df['start'].str.split(':', expand=True).astype(int)
+    s = pd.to_timedelta(v[0], unit='h') + pd.to_timedelta(v[1], unit='s')
+    df['start'] = s.astype(int)
+
+    v = df['finish'].str.split(':', expand=True).astype(int)
+    s = pd.to_timedelta(v[0], unit='h') + pd.to_timedelta(v[1], unit='s')
+    df['finish'] = s.astype(int)
+
+
+def convert_gps(df):
+    df.ffill(axis=1, inplace=True)
+    df['X'] = df["x"].astype(float)
+    df['Y'] = df["y"].astype(float)
+    print(df)
+
+
 def main():
     data_op = 'data/operations.csv'
     df = pd.read_csv(data_op, parse_dates=True, error_bad_lines=False)
 
-    print(df)
-
     dict_typology(df)
     dict_loc(df)
+    convert_time(df)
+    # convert_gps(df)
 
     df = extract_feature(df)
 
-    print(df.head(15))
+    print(df.dtypes)
+    print(df)
+    corr = df.corr()
+
+    plt.figure(figsize=(12, 12))
+    sns.heatmap(corr, cmap="YlOrRd", annot=True, linewidths=0.3, vmin=-1, square=False)
+    plt.show()
 
 
 if __name__ == "__main__":
