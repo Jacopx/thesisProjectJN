164e400193a566690c980610fd11084adfe3aa94
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-10-02 15:02:29 +0200

    Some corrections over documentation, adding name field inside process and fixing docker compose and read access issue

--- CommonDB/main.py
+++ CommonDB/main.py
@@ -4,13 +4,12 @@ import pandas as pd
 import time
 import sys
 
+# Docker
 USER = 'user'
 DB = 'forecast'
 PWD = 'userpwd'
 HOST = 'localhost'
-
-# NAME = 'SFFD'
-NAME = 'SFBS'
+PORT = '3306'
 
 column_link = {}
 link_column = {}
@@ -18,7 +17,7 @@ BASE_PATH = 'data_to_load/'
 
 
 def connect():
-    return mysql.connector.connect(host=HOST, user=USER, passwd=PWD, database=DB)
+    return mysql.connector.connect(host=HOST, port=PORT, user=USER, passwd=PWD, database=DB)
 
 
 def check_table_exist(dbc, tablename):
@@ -36,29 +35,29 @@ def check_table_exist(dbc, tablename):
 def create_table(dbc):
     c = dbc.cursor()
 
-    # if check_table_exist(dbc, 'data'):
-    #     print('Table DATA already present...')
-    #     c.execute('DROP TABLE data')
-    #     print('Drop DATA table...')
-    #
-    # print('Creating DATA table...')
-    # c.execute("CREATE TABLE data ("
-    #           "id INT AUTO_INCREMENT PRIMARY KEY,"
-    #           "name VARCHAR(255),"
-    #           "start_dt DATETIME,"
-    #           "end_dt DATETIME,"
-    #           "duration INT,"
-    #           "type VARCHAR(100),"
-    #           "feat1 VARCHAR(100),"
-    #           "feat2 VARCHAR(100),"
-    #           "feat3 VARCHAR(100),"
-    #           "feat4 VARCHAR(100),"
-    #           "feat5 VARCHAR(100),"
-    #           "feat6 VARCHAR(100),"
-    #           "feat7 VARCHAR(100),"
-    #           "feat8 VARCHAR(100),"
-    #           "feat9 VARCHAR(100),"
-    #           "feat10 VARCHAR(100))")
+    if check_table_exist(dbc, 'data'):
+        print('Table DATA already present...')
+        c.execute('DROP TABLE data')
+        print('Drop DATA table...')
+
+    print('Creating DATA table...')
+    c.execute("CREATE TABLE data ("
+              "id INT AUTO_INCREMENT PRIMARY KEY,"
+              "name VARCHAR(255),"
+              "start_dt DATETIME,"
+              "end_dt DATETIME,"
+              "duration INT,"
+              "type VARCHAR(100),"
+              "feat1 VARCHAR(100),"
+              "feat2 VARCHAR(100),"
+              "feat3 VARCHAR(100),"
+              "feat4 VARCHAR(100),"
+              "feat5 VARCHAR(100),"
+              "feat6 VARCHAR(100),"
+              "feat7 VARCHAR(100),"
+              "feat8 VARCHAR(100),"
+              "feat9 VARCHAR(100),"
+              "feat10 VARCHAR(100))")
 
     if check_table_exist(dbc, 'dataset'):
         print('Table DATASET already present...')
@@ -104,12 +103,12 @@ def rename_col(df):
     df.rename(columns=column_link, inplace=True)
 
 
-def load_to_db(df, dbc):
+def load_to_db(name, df, dbc):
     c = dbc.cursor()
 
     sql = "INSERT INTO dataset(name, descr, feat1, feat2, feat3, feat4, feat5, feat6," \
           "feat7, feat8, feat9, feat10) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (NAME, 'prova',
+    val = (name, 'prova',
            link_column['feat1'], link_column['feat2'], link_column['feat3'], link_column['feat4'], link_column['feat5'],
            link_column['feat6'], link_column['feat7'], link_column['feat8'], link_column['feat9'], link_column['feat10'])
 
@@ -137,7 +136,7 @@ def load_to_db(df, dbc):
 
     for i, row in df.iterrows():
         t = list(row)
-        t.insert(0, NAME)
+        t.insert(0, name)
 
         try:
             c.execute(sql, tuple(t))  # Syntax error in query
@@ -151,20 +150,20 @@ def load_to_db(df, dbc):
             dbc.commit()
 
         # Early stopper for debug
-        # if i == 2000000:
-        #     break
+        if i == 2000000:
+            break
 
     dbc.commit()
     print('\nError line #{}'.format(error))
     print("Execution time [{} s]".format(round(time.time()-t0, 2)))
 
 
-def main(data, dict):
+def main(name, data, dict):
     # Database Connector
     dbc = connect()
 
     # Creating Tables
-    create_table(dbc)
+    # create_table(dbc)
 
     # Reading the dictionary used for matching
     read_dict(dict)
@@ -172,8 +171,8 @@ def main(data, dict):
     # Loading data on DB
     df = loading_csv(data)
     rename_col(df)
-    load_to_db(df, dbc)
+    load_to_db(name, df, dbc)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
