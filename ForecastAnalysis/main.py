d9ebe8d70032e5aa6481c52315f2c5d91263d924
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-10-11 17:14:19 +0200

    Adding duration computation

--- ForecastAnalysis/main.py
+++ ForecastAnalysis/main.py
@@ -8,16 +8,19 @@ import sys
 
 # FreeNAS
 USER = 'eis'
-DB = 'forecastDev'
+DB = 'forecast'
 PWD = 'eisworld2019'
 HOST = 'db.jacopx.me'
-PORT = '3306'
+PORT = '33306'
 
 
 def connect(): return mysql.connector.connect(host=HOST, port=PORT, user=USER, passwd=PWD, database=DB)
 
 
 def distance(a, b): return geopy.distance.geodesic(a, b).m
+
+
+def duration(s, e): return e-s;
 
 
 def generate_df(dbc, sql): return pd.read_sql(sql, dbc)
@@ -36,9 +39,8 @@ def bs_distance(dbc, dataset):
           "AND involved.type=('{}') " \
           "AND object.type=('{}') " \
           "ORDER BY event.eid " \
-          "LIMIT 6;".format(dataset, dataset, 'src', 'passive')
+          "LIMIT 50000;".format(dataset, dataset, 'src', 'passive')
     df_src = generate_df(dbc, sql)
-    print(df_src)
 
     sql = "SELECT event.eid, involved.type, object.id as e_id, latitude as e_lat, longitude as e_long " \
           "FROM event, involved, object, location " \
@@ -52,13 +54,23 @@ def bs_distance(dbc, dataset):
           "AND involved.type=('{}') " \
           "AND object.type=('{}') " \
           "ORDER BY event.eid " \
-          "LIMIT 6;".format(dataset, dataset, 'dest', 'passive')
+          "LIMIT 50000;".format(dataset, dataset, 'dest', 'passive')
     df_dst = generate_df(dbc, sql)
-    print(df_dst)
 
     df = pd.merge(df_dst, df_src, on=['eid'])
     df = df[['eid', 's_id', 's_lat', 's_long', 'e_id', 'e_lat', 'e_long']]
-    df['distance'] = df.apply(lambda x: distance((x['s_lat'], x['s_long']), (x['e_lat'], x['e_long'])), axis=1)
+    df['distance'] = df.apply(lambda row: distance((row['s_lat'], row['s_long']), (row['e_lat'], row['e_long'])), axis=1)
+    return df
+
+
+def bs_duration(dbc, dataset):
+    sql = "SELECT eid, start_dt, end_dt " \
+          "FROM event " \
+          "WHERE event.dataset=('{}') " \
+          "ORDER BY event.eid " \
+          "LIMIT 50000;".format(dataset)
+    df = generate_df(dbc, sql)
+    df['duration'] = df.apply(lambda row: duration(row['start_dt'], row['end_dt']), axis=1)
     return df
 
 
@@ -67,6 +79,9 @@ def main(dataset):
     dbc = connect()
 
     df = bs_distance(dbc, dataset)
+    print(df)
+
+    df = bs_duration(dbc, dataset)
     print(df)
 
 
