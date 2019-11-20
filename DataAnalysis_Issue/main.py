47b738f044b81bf44d7f2b390ff48b4f23a7abb3
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-11-20 15:50:42 +0100

    Adding different folder path based on execution argument

--- DataAnalysis_Issue/main.py
+++ DataAnalysis_Issue/main.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 
 def merge(dataset):
-    issue = pd.read_csv('data/issue.csv', nrows=None, parse_dates=True)
-    issue = issue.drop('description', axis=1)
+    issue = pd.read_csv('data/' + dataset + '/issue.csv', nrows=None, parse_dates=True)
+    # issue = issue.drop('description', axis=1)
     issue = issue.drop('created_date', axis=1)
     issue = issue.drop('updated_date', axis=1)
     issue = issue.drop('resolved_date', axis=1)
 
-    change_set = pd.read_csv('data/change_set.csv', nrows=None, parse_dates=True)
+    change_set = pd.read_csv('data/' + dataset + '/change_set.csv', nrows=None, parse_dates=True)
     change_set = change_set.drop('committed_date', axis=1)
     change_set = change_set.drop('is_merge', axis=1)
 
-    change_link = pd.read_csv('data/change_set_link.csv', nrows=None, parse_dates=True)
+    change_link = pd.read_csv('data/' + dataset + '/change_set_link.csv', nrows=None, parse_dates=True)
 
-    fix_version = pd.read_csv('data/issue_fix_version.csv', nrows=None, parse_dates=True)
+    fix_version = pd.read_csv('data/' + dataset + '/issue_fix_version.csv', nrows=None, parse_dates=True)
 
-    code_change = pd.read_csv('data/code_change.csv', nrows=None, parse_dates=True)
+    code_change = pd.read_csv('data/' + dataset + '/code_change.csv', nrows=None, parse_dates=True)
     code_change = code_change.drop('file_path', axis=1)
     code_change = code_change.drop('old_file_path', axis=1)
 
